955094c47afbb47bdb634482d283a347f90aada2
ANKITMAGGU9 <144800538+ANKITMAGGU9@users.noreply.github.com>
2023-12-07 13:48:34 +0530

    Update 4_SPI_Recieve.ino

--- 4_SPI_Recieve.ino
+++ 4_SPI_Recieve.ino
@@ -1,31 +1,20 @@
-// include the library code:
-#include <LiquidCrystal.h>
+#include <SPI.h>
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-int cursor = 0;
+const int slaveSelectPin = 10; 
+
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  //lcd.print("hello, world!");
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0)); 
+
+  pinMode(slaveSelectPin, INPUT); 
+  digitalWrite(slaveSelectPin, HIGH); 
   Serial.begin(9600);
-  lcd.setCursor(cursor, 0);
 }
 
-void loop() { 
-  if (Serial.available() > 0) {
-    char data = Serial.read();
-    lcd.print(data);
-    cursor++;
-    lcd.setCursor(cursor, 0);
+void loop() {
+  if (digitalRead(slaveSelectPin) == LOW) {
+    char receivedData = SPI.transfer(0); // Receive data from the master
 
-    // Reset the cursor and clear the display after the cursor limit is reached.
-    if (cursor > 16) {
-      cursor = 0;
-      lcd.clear();
-    }
+    Serial.print(receivedData);
   }
 }
